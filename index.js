2716efa08b1035450b564b93be1567ad4a546f4c
manu <100472310@alumnos.uc3m.es>
2024-04-05 11:51:23 +0200

    Sockets limpios, ficheros de funcionalidades añadidas

--- index.js
+++ index.js
@@ -11,23 +11,18 @@ let clientSocket;
 io.on('connection', (socket) => {
   console.log(`socket connected ${socket.id}`);
 
-  socket.on("POINTER_CONNECTED", () => {
-    socket.emit("ACK_CONNECTION");
-    if (clientSocket) clientSocket.emit("NEW_POINTER", { pointerId: socket.id });
-  });
-
+  socket.on("CLIENT_CONNECTED", () => {
+    clientSocket = socket;
+    clientSocket.emit("ACK_CONNECTION");
+  })
+  
   socket.on("SENSOR_READING", (data) => {
-    //console.log(data);
     if (clientSocket) clientSocket.emit("SENSOR_READING", {
       pointerId: socket.id,
       coords: data
     });
   });
 
-  socket.on("CLIENT_CONNECTED", () => {
-    clientSocket = socket;
-    clientSocket.emit("ACK_CONNECTION");
-  })
 });
 
 server.listen(3000, () => {
