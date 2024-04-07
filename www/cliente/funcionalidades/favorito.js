17b4a350df877efc50ff45aff2a2a46f8e49974a
manu <100472310@alumnos.uc3m.es>
2024-04-07 20:45:47 +0200

    cambios en fav para poder hacer click

--- www/cliente/funcionalidades/favorito.js
+++ www/cliente/funcionalidades/favorito.js
@@ -3,10 +3,11 @@ var favorito = false;
 var contador = 0;
 var obj_fav = false;
 const star = document.getElementsByClassName("star")[0];
+const text_fav = star.children[0];
 
 // Listener para el favorito
 window.addEventListener("devicemotion", handle_pos);
-
+star.addEventListener("click", change_obj_fav);
 export function change_fav() {
 	favorito = !favorito;
 }
@@ -41,6 +42,7 @@ function trigger_star_appearing() {
 	star.style.animation = "appear_star 1s 1";
 	window.setTimeout(() =>{
 		star.style.backgroundColor = "yellow";
+		text_fav.innerHTML = "Favorito";
 	}, 990);
 }
 
@@ -48,5 +50,7 @@ function trigger_star_disappearing() {
 	star.style.animation = "disappear_star 1s 1";
 	window.setTimeout(() => {
 		star.style.backgroundColor = "transparent";
+		text_fav.innerHTML = "No favorito";
+		
 	}, 990);
 }
