4d098208d4417d8f7e3111c41b6826a5cdbc1d35
manu <100472310@alumnos.uc3m.es>
2024-04-07 19:58:32 +0200

    añadida animación de cambio de favorito

--- www/cliente/funcionalidades/favorito.js
+++ www/cliente/funcionalidades/favorito.js
@@ -28,7 +28,7 @@ function handle_pos(ev){
 }
 
 function change_obj_fav() {
-	if (!obj_false) {
+	if (!obj_fav) {
 		trigger_star_appearing();
 	}
 	else {
@@ -38,9 +38,15 @@ function change_obj_fav() {
 }
 
 function trigger_star_appearing() {
-	
+	star.style.animation = "appear_star 1s 1";
+	window.setTimeout(() =>{
+		star.style.backgroundColor = "yellow";
+	}, 990);
 }
 
 function trigger_star_disappearing() {
-
+	star.style.animation = "disappear_star 1s 1";
+	window.setTimeout(() => {
+		star.style.backgroundColor = "transparent";
+	}, 990);
 }
