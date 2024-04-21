4197e6d865ebe41404b609d92f90c47a346d4726
Adrian <100472182@alumnos.uc3m.es>
2024-04-21 20:39:55 +0200

    era broma

--- www/cliente/funcionalidades/eliminar.js
+++
@@ -1,9 +0,0 @@
-export var div_id_eliminar;
-
-window.addEventListener("deviceorientation", handleOrientation, true);
-function handleOrientation(event) {
-    var beta = event.beta;
-
-    if (beta > 170 && beta < 190) {
-    }
-}
